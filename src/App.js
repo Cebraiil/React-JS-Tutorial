b85d2f33a7f45de5c8d757a75f405a4245759371
developerlight <abdul.adl@icloud.com>
2022-08-22 08:28:56 +0300

    React Js Tutorial #3 adding Home header

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='home-header'>
+        <div className='content'>
+          <div className='text'>
+            <p> Hi I'm gabriel, I'm a web and mobile programming expert. I am currently
+                     working as a Web software development at Developer Light company.</p>
+          </div>
+          <div className='actions'>
+            <a href="" className='btn-about'>About Me</a>
+            <a href="" className='btn-twitter'>Follow me on Twitter</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
